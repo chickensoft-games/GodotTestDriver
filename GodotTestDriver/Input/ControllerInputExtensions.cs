8e98099f8c5b69ea9e48bd9be51ed9bc107c8431
Mark Wilson <23439518+wlsnmrk@users.noreply.github.com>
2024-03-27 13:18:53 -0400

    feat: add controller extensions to hold input for duration

--- GodotTestDriver/Input/ControllerInputExtensions.cs
+++ GodotTestDriver/Input/ControllerInputExtensions.cs
@@ -1,6 +1,8 @@
 namespace Chickensoft.GodotTestDriver.Input;
 
 using System;
+using System.Threading.Tasks;
+using Chickensoft.GodotTestDriver.Util;
 using Godot;
 using JetBrains.Annotations;
 
@@ -48,6 +50,22 @@ public static class ControllerInputExtensions
     }
 
     /// <summary>
+    /// Hold a two-sided analog-axis controller input for a given duration.
+    /// </summary>
+    /// <param name="node">Node that generates simulated input.</param>
+    /// <param name="seconds">Time, in seconds.</param>
+    /// <param name="negativeAction">Action name for the negative side of the axis.</param>
+    /// <param name="positiveAction">Action name for the positive side of the axis.</param>
+    /// <param name="axisPosition">The position of the axis input, from -1 to +1.</param>
+    /// <returns>Task that completes when the input finishes.</returns>
+    public static async Task HoldBidirectionalAxisInputFor(this Node node, float seconds, string negativeAction, string positiveAction, float axisPosition)
+    {
+        node.SetBidirectionalAxisInput(negativeAction, positiveAction, axisPosition);
+        await node.Wait(seconds);
+        node.EndBidirectionalAxisInput(negativeAction, positiveAction);
+    }
+
+    /// <summary>
     /// Set a simulated input value for a one-sided analog axis on a controller (for instance, a gamepad trigger).
     /// </summary>
     /// <param name="node">Node that generates simulated input.</param>
@@ -74,5 +92,20 @@ public static class ControllerInputExtensions
     public static void EndSingleAxisInput(this Node node, string action)
     {
         node.EndAction(action);
+    }
+
+    /// <summary>
+    /// Hold a one-sided analog-axis controller input for a given duration.
+    /// </summary>
+    /// <param name="node">Node that generates simulated input.</param>
+    /// <param name="seconds">Time, in seconds.</param>
+    /// <param name="action">Action name for the axis.</param>
+    /// <param name="axisPosition">The position of the axis input, from 0 to 1.</param>
+    /// <returns>Task that completes when the input finishes.</returns>
+    public static async Task HoldSingleAxisInputFor(this Node node, float seconds, string action, float axisPosition)
+    {
+        node.SetSingleAxisInput(action, axisPosition);
+        await node.Wait(seconds);
+        node.EndSingleAxisInput(action);
     }
 }
