4d5a4a7d6823f7eb62714b60ed41f35a93cf0810
Mark Wilson <23439518+wlsnmrk@users.noreply.github.com>
2024-03-26 23:45:34 -0400

    refactor: improve controller-input method names
    Renamed "Start*AxisInput" methods to "Set*AxisInput", since analog
    axis inputs don't echo and can set new strength values without
    ending or re-firing.

--- GodotTestDriver/Input/ControllerInputExtensions.cs
+++ GodotTestDriver/Input/ControllerInputExtensions.cs
@@ -9,14 +9,14 @@ using JetBrains.Annotations;
 public static class ControllerInputExtensions
 {
     /// <summary>
-    /// Start a simulated input for a two-sided analog axis on a controller (for instance, the x-axis of a thumbstick).
+    /// Set a simulated input value for a two-sided analog axis on a controller (for instance, the x-axis of a thumbstick).
     /// </summary>
     /// <param name="node">Node that generates simulated input.</param>
     /// <param name="negativeAction">Action name for the negative side of the axis.</param>
     /// <param name="positiveAction">Action name for the positive side of the axis.</param>
     /// <param name="axisPosition">The position of the axis input, from -1 to +1.</param>
     /// <seealso cref="Input.GetAxis(StringName, StringName)"/>
-    public static void StartBidirectionalAxisInput(this Node node, string negativeAction, string positiveAction, float axisPosition)
+    public static void SetBidirectionalAxisInput(this Node node, string negativeAction, string positiveAction, float axisPosition)
     {
         var action = positiveAction;
         if (axisPosition < 0)
@@ -40,13 +40,13 @@ public static class ControllerInputExtensions
     }
 
     /// <summary>
-    /// Start a simulated input for a one-sided analog axis on a controller (for instance, a gamepad trigger).
+    /// Set a simulated input value for a one-sided analog axis on a controller (for instance, a gamepad trigger).
     /// </summary>
     /// <param name="node">Node that generates simulated input.</param>
     /// <param name="action">Action name for the axis.</param>
     /// <param name="axisPosition">The position of the axis input, from 0 to 1.</param>
     /// <seealso cref="Input.GetActionStrength(StringName, bool)"/>
-    public static void StartSingleAxisInput(this Node node, string action, float axisPosition)
+    public static void SetSingleAxisInput(this Node node, string action, float axisPosition)
     {
         node.StartAction(action, axisPosition);
     }
